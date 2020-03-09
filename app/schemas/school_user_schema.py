1b839abfe7193d8a451f56acec26fe9a3a8de20f
Greudys Godoy <greudys@binauraldev.com>
2020-03-09 11:49:10 -0400

    MOD. Agregados campos adicionales a endpoint escuelas #84

--- app/schemas/school_user_schema.py
+++ app/schemas/school_user_schema.py
@@ -6,8 +6,9 @@ from marshmallow import validate, EXCLUDE
 from app.schemas.user_schema import UserSchema
 from app.schemas.shared_schemas import ProjectReferenceSchema
 from app.schemas import fields
+from app.helpers.ma_schema_fields import MAImageField
 from app.helpers.ma_schema_validators import (
-    not_blank, only_letters, only_numbers, OneOf)
+    not_blank, only_letters, only_numbers, OneOf, Range, validate_image)
 
 
 class SchoolUserSchema(UserSchema):
@@ -28,6 +29,33 @@ class SchoolUserSchema(UserSchema):
         required=True,
         validate=(not_blank, only_numbers))
     contactFunction = fields.Str(required=True)
+    image = MAImageField(validate=validate_image,
+                         folder='schools')
+    schoolType = fields.Str(
+        validate=OneOf(
+            ('1', '2', '3'),
+            ('national', 'statal', 'municipal')
+        ))
+    principalFirstName = fields.Str()
+    principalLastName = fields.Str()
+    principalEmail = fields.Email()
+    principalPhone = fields.Str(validate=only_numbers)
+    subPrincipalFirstName = fields.Str()
+    subPrincipalLastName = fields.Str()
+    subPrincipalEmail = fields.Email()
+    subPrincipalPhone = fields.Str(validate=only_numbers)
+    nTeachers = fields.Int(validate=Range(min=0))
+    nAdministrativeStaff = fields.Int(
+        validate=Range(min=0))
+    nLaborStaff = fields.Int(validate=Range(min=0))
+    nStudents = fields.Int(validate=Range(min=0))
+    nGrades = fields.Int(validate=Range(min=0))
+    nSections = fields.Int(validate=Range(min=0))
+    schoolShift = fields.Str(
+        validate=OneOf(
+            ('1', '2', '3'),
+            ('morning', 'afternoon', 'both')
+        ))
     status = fields.Str(
         validate=OneOf(
             ("1", "2", "3"),

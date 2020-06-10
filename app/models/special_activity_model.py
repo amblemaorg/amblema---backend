af4e9f7df2d2087932c3f3658812c6b261ca7732
Yasser Quiñonez <yasser@binaural.com.ve>
2020-06-10 09:28:03 -0400

    IMP: desarrollo de crud para las actividades especiales. Archivos modificados special_activity_model.py, special_activity_schema.py. Issue #123

--- app/models/special_activity_model.py
+++ app/models/special_activity_model.py
@@ -6,11 +6,24 @@ from bson import ObjectId
 
 from flask import current_app
 from mongoengine import fields, EmbeddedDocument
-from app.models.item_special_activity_model import ItemSpecialActivity
+
+
+class ItemSpecialActivity(EmbeddedDocument):
+    id = fields.ObjectIdField(default=fields.ObjectId)
+    name = fields.StringField(required=True)
+    description = fields.StringField(required=True)
+    quantity = fields.IntField(default=0.0)
+    unitPrice = fields.FloatField(default=0.0)
+    tax = fields.FloatField(default=0.0)
+    subtotal = fields.FloatField(default=0.0)
+    isDeleted = fields.BooleanField(default=False)
+    createdAt = fields.DateTimeField(default=datetime.utcnow)
+    updatedAt = fields.DateTimeField(default=datetime.utcnow)
+
 
 class SpecialActivity(EmbeddedDocument):
     id = fields.ObjectIdField(default=fields.ObjectId)
-    name = fields.StringField(default="Actividad especial")
+    name = fields.StringField(default="Actividad especial de lapso")
     activityDate = fields.DateTimeField(required=True)
     # approvalStatus = ("1": "pending", "2": "approved", "3": "rejected", "4": "cancelled")
     approvalStatus = fields.StringField(default='1', max_length=1)

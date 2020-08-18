557e35452ed56852d6a07ecad30f04dfbcb9ede7
Greudys Godoy <greudys@binauraldev.com>
2020-08-18 15:56:34 -0400

    FIX actividades sin descripcion en anuario

--- app/schemas/peca_activity_yearbook_schema.py
+++ app/schemas/peca_activity_yearbook_schema.py
@@ -11,7 +11,7 @@ from app.models.peca_activity_yearbook_model import ActivityYearbook
 class ActivityYearbookSchema(Schema):
     id = fields.Str(dump_only=True)
     name = fields.Str(dump_only=True)
-    description = fields.Str()
+    description = fields.Str(allow_none=True)
     images = fields.List(fields.Str())
 
     class Meta:

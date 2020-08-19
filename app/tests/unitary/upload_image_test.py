2a3bf9d9b1609822f00ec6432f02a8e4523e75b5
Greudys Godoy <greudys@binauraldev.com>
2020-08-19 17:03:47 -0400

    IMP libreria pillow para manejo de imagenes

---
+++ app/tests/unitary/upload_image_test.py
@@ -0,0 +1,27 @@
+# app/tests/unitary/learning_module_test.py
+
+import unittest
+import json
+
+from app import create_app
+from app.schemas.learning_module_schema import LearningModuleSchema
+from resources.images import test_image, jpgImage, pngImage
+from app.helpers.handler_images import upload_image
+
+
+class LearningModuleUnitaryTestCase(unittest.TestCase):
+    """Test case for learning module validations."""
+
+    def setUp(self):
+        self.app = create_app(config_instance="testing")
+        self.app.app_context().push()
+        
+
+    def test_upload_image(self):
+        self.app.logger.info(upload_image(test_image, 'tests'))
+        self.app.logger.info(upload_image(jpgImage, 'tests'))
+        self.app.logger.info(upload_image(pngImage, 'tests'))
+
+        
+
+    

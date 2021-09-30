13c44673321ddfd7f6852d51c17ea3e6b4de1df1
ysabel <ysabel.godoy@binauraldev.com>
2021-09-29 22:27:13 -0400

    WIP: Promover lsita de estudiantes

--- app/services/promote_student_service.py
+++ app/services/promote_student_service.py
@@ -8,7 +8,7 @@ from app.models.school_year_model import SchoolYear
 from app.models.peca_project_model import PecaProject
 from app.helpers.error_helpers import RegisterNotFound
 from app.models.school_user_model import SchoolUser
-from app.models.peca_student_model import SectionClass
+from app.models.peca_student_model import SectionClass, Student
 from app.models.peca_student_model import StudentClass
 
 class PromoteStudentService():
@@ -29,6 +29,41 @@ class PromoteStudentService():
         if section_peca:
             students_list = [{"id":str(student.id), "firstName": student.firstName, "lastName": student.lastName, "cardId": student.cardId, "cardType": student.cardType, "birthdate": str(student.birthdate), "gender": student.gender} for student in section_peca.students]
         return {"status":200, "msg": "Exito", "students": students_list},200
+    
+    def promoteStudents(self, school_code, id_section_previus, id_section_current, students, pecaId):
+        schoolYear = SchoolYear.objects(isDeleted=False, status="1").first()
+        peca_actual = PecaProject.objects(
+            isDeleted=False,
+            schoolYear=schoolYear.id,).first()
+        section = peca_actual.school.sections.filter(
+                isDeleted=False, id=id_section_current).first()
+        if section:
+
+            for student in students:
+                student_save = Student()
+                student_save.firstName = student.firstName
+                student_save.lastName = student.lastName
+                student_save.cardId = student.cardId
+                student_save.cardType = student.cardType
+                student_save.birthdate = student.birthdate
+                student_save.gender = student.gender
+                student_save.isDeleted = False
+                nStudents = 1
+                for section in peca.school.sections.filter(isDeleted=False):
+                    nStudents += len(section.students.filter(isDeleted=False))
+                        
+                PecaProject.objects(
+                            id=pecaId,
+                            school__code=school_code,
+                            school__sections__id=id_section_current
+                        ).update(
+                            push__school__sections__S__students=student_save,
+                            set__school__nStudents=nStudents)
+            
+            return {"status":201, "msg": "Estudiantes promovidos con exito"},201
+        else:
+            return {"status":400, "msg": "La sección no existe"},201
+        
 
 class SectionsPromoteStudentService():
     def getSections(self, school_code):

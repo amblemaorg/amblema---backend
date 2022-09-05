99513ad7e97f66fec18033b2586d61a58858bb05
Ander Leon <anderson@binauraldev.com>
2022-09-05 14:22:41 -0400

    fix: actualizar campos de impresion
    *Se añadio obtener los datos de impresion mediante el get
    
    *se pueden actualizar los registros de yearbook mediante patch

--- app/models/peca_yearbook_model.py
+++ app/models/peca_yearbook_model.py
@@ -10,8 +10,8 @@ from app.models.shared_embedded_documents import Approval
 
 
 class PrintOption(EmbeddedDocument):
-    print = fields.BooleanField(default=False)
-    expandGallery = fields.BooleanField(default=False)
+    print = fields.BooleanField(default=True)
+    expandGallery = fields.BooleanField(default=True)
 class Entity(EmbeddedDocument):
     name = fields.StringField()
     image = fields.StringField(null=True)
@@ -19,13 +19,18 @@ class Entity(EmbeddedDocument):
     printOption = fields.EmbeddedDocumentField(PrintOption, default=PrintOption())
 
 class Index(EmbeddedDocument):
-    print = fields.BooleanField(default=False)
+    print = fields.BooleanField(default=True)
 class Lapse(EmbeddedDocument):
     readingDiagnosticAnalysis = fields.StringField(default='')
     mathDiagnosticAnalysis = fields.StringField(default='')
     logicDiagnosticAnalysis = fields.StringField(default='')
     printOption = fields.EmbeddedDocumentField(PrintOption, default=PrintOption())
 
+class Activity(EmbeddedDocument):
+    name = fields.StringField(default='')
+    print = fields.BooleanField(default=False)
+    expandGallery = fields.BooleanField(default=False)
+    lapse = fields.StringField(default='')
 class Yearbook(EmbeddedDocument):
     historicalReview = fields.EmbeddedDocumentField(Entity, default=Entity())
     sponsor = fields.EmbeddedDocumentField(Entity, default=Entity())
@@ -35,6 +40,7 @@ class Yearbook(EmbeddedDocument):
     lapse2 = fields.EmbeddedDocumentField(Lapse, default=Lapse())
     lapse3 = fields.EmbeddedDocumentField(Lapse, default=Lapse())
     index = fields.EmbeddedDocumentField(Index, default=Index())
+    activitiesPrint = fields.ListField(fields.EmbeddedDocumentField(Activity, default=Activity()))
     isInApproval = fields.BooleanField(default=False)
     approvalHistory = fields.EmbeddedDocumentListField(Approval)
     updatedAt = fields.DateTimeField(default=datetime.utcnow)

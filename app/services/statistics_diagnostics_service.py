27c23eeb6f1bdba44be2e14d7e423e096bc97e8c
alejosfidel <fidel.alejos@gmail.com>
2024-04-04 18:56:46 -0400

    ajuste summary reporte diagnosticos

--- app/services/statistics_diagnostics_service.py
+++ app/services/statistics_diagnostics_service.py
@@ -120,8 +120,13 @@ class StatisticsDiagnosticService():
                                         sectionLapse[diag]['participants'] += 1
                                         sectionLapse[diag]['resultTotal'] += studentLapse[diagnostics[diag]]
                                         sectionLapse[diag]['indexTotal'] += float(studentLapse['{}Index'.format(diagnostics[diag])]) if studentLapse['{}Index'.format(diagnostics[diag])]!=None else 0
-                                        if studentLapse[diagnostics[diag]] > goal:
-                                            sectionLapse[diag]['overGoalStudents'] += 1
+                                        if diag == "reading":
+                                            if studentLapse[diagnostics[diag]] >= goal:
+                                                sectionLapse[diag]['overGoalStudents'] += 1
+
+                                        if diag in ["math", "logic"]:
+                                            if studentLapse[diagnostics[diag]] == goal:
+                                                sectionLapse[diag]['overGoalStudents'] += 1
 
                                         # set dates of diagnostics
                                         diagnosticsDates['lapse{}'.format(i+1)][diag].append(

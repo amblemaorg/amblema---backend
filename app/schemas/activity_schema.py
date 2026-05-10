fde1da1b63088320092c82c5f5095eeb52922c24
Fidel Aliaga <fidel.alejos@gmail.com>
2026-05-10 15:25:59 -0400

    caroursel de actividades en detalle de escuela

--- app/schemas/activity_schema.py
+++ app/schemas/activity_schema.py
@@ -20,7 +20,7 @@ class ActivitySchema(Schema):
     id = fields.Str(dump_only=True)
     name = fields.Str(required=True, validate=not_blank)
     devName = fields.Str(dump_only=True)
-    description = fields.Str(allow_none=True, validate=Length(max=100))
+    description = fields.Str(allow_none=True, validate=Length(max=300))
     hasText = fields.Bool(required=True, default=False)
     hasDate = fields.Bool(required=True, default=False)
     hasFile = fields.Bool(required=True, default=False)
